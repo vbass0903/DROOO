3617ca833662a184a7589e5b305b4f1c88d917b2
vbass <32740596+vbass0903@users.noreply.github.com>
2020-02-05 11:31:37 -0600

    Player Camera Follow + WIP Ship Station System
    WIP Ship Station: Added public vars isAttached and attachedStation, change on station collision.

---
+++ CS377-DROOO/Assets/Scripts/stationControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stationControl : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Debug.Log("Press F to pay Respects");
+            Player.GetComponent<playerMove>().attachedStation = gameObject.name;
+            Player.GetComponent<playerMove>().isAttached = true;
+        }
+    }
+}
