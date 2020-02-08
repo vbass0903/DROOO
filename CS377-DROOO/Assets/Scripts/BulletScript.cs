712667a838c997acf09cc77f531ed341797c68c7
zackloch <zacharylochmueller2017@u.northwestern.edu>
2020-02-08 05:06:07 -0600

    Turrets + Bullets

---
+++ CS377-DROOO/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public Rigidbody2D bullet;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+}
---
+++ CS377-DROOO/Assets/Scripts/TurretGun2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretGun2 : MonoBehaviour
+{
+    GameObject Player;
+    GameObject Turret;
+    GameObject Body;
+
+    Vector3 movement;
+
+    public Rigidbody2D Bullet;
+    public Rigidbody2D new_Bullet;
+
+    public float speed;
+    public float timeBetweenShots;
+    public float timeDestroy;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        Turret = GameObject.Find("TurretGun2");
+        Body = GameObject.Find("TurretBody2");
+ 
+        speed = 10f;
+        timeBetweenShots = 0.5f;
+        timeDestroy = 3f;
+    }
+
+    void Update()
+    {
+        movement = new Vector3(0f, 0f, Input.GetAxis("Horizontal"));
+        if (Input.GetKeyDown("w") && Player.GetComponent<playerStation>().isLeftTurret)
+        {
+            new_Bullet = Instantiate(Bullet, Turret.transform.position, Turret.transform.rotation);
+            new_Bullet.velocity = transform.right * speed;
+            Destroy(new_Bullet.gameObject, timeDestroy);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            Turret.transform.RotateAround(Body.transform.position, Vector3.forward, 100f * Time.deltaTime);
+        }
+        if (collision.CompareTag("Wall"))
+        {
+            Turret.transform.RotateAround(Body.transform.position, Vector3.back, 100f * Time.deltaTime);
+        }
+
+    }
+
+
+
+}
